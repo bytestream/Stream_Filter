78599f30e842a646885ca130d18f6679a28c8d12
Kieran Brahney <kieran@supportpal.com>
2021-12-20 22:47:31 +0000

    PHP 8 support

--- test/Horde/Stream/Filter/Bin2hexTest.php
+++ test/Horde/Stream/Filter/Bin2hexTest.php
@@ -10,13 +10,14 @@
  * @package    Stream_Filter
  * @subpackage UnitTests
  */
-class Horde_Stream_Filter_Bin2hexTest extends Horde_Test_Case
+class Horde_Stream_Filter_Bin2hexTest extends \PHPUnit\Framework\TestCase
 {
     public $fp;
     public $testdata;
 
-    public function setup()
+    public function setUp(): void
     {
+	parent::setUp();
         stream_filter_register('horde_bin2hex', 'Horde_Stream_Filter_Bin2hex');
 
         $this->testdata = str_repeat("0123456789ABCDE", 1000);
